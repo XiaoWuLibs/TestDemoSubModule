e2b98c6a1b89e43ab91a5ac31c15d6ba77d432a1
wangshuolei <15731137350@126.con>
2021-11-04 11:18:55 +0800

    提交1

--- src/main/java/com/study/testdemosubmodule/MainActivity.java
+++ src/main/java/com/study/testdemosubmodule/MainActivity.java
@@ -2,13 +2,25 @@ package com.study.testdemosubmodule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     * 启动Activity
+     *
+     * @param context
+     */
+    public static void startActivity(Context context) {
+        Intent intent = new Intent();
+        intent.setClass(context, MainActivity.class);
+        context.startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_submodule);
     }
 }
